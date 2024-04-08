a0ed235eaaa666a7735235ad4f326fe19dea3718
Sithanga Rashmika Sandaruwan <87372439+IT21041716@users.noreply.github.com>
2024-04-08 23:38:00 +0530

    project envirinment done

---
+++ Blind_Safe/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text} >Welcome huttooo this is the begining of our pake research..!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 20,
+    textAlign:'center',
+  },
+});
